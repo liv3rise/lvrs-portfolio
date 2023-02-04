ae863be3f6bf17ce77bf7d3bc89d4809eaf321e1
liv3rise <liv3rise@gmail.com>
2023-02-04 09:19:36 +0200

    contact section done

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 import '../scss/styles.scss'
 
+const messageInput = document.getElementById('messageInput');
+
+// messageInput.addEventListener('input', (e) => {
+//     e.target.style.height = e.target.scrollHeight + 'px';
+// })
