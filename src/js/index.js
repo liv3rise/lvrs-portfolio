fdeaa8d8b4295577660e42408febceeb5e5b9567
liv3rise <liv3rise@gmail.com>
2023-02-07 08:57:04 +0200

    theme switching done

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,43 @@
-import '../scss/styles.scss'
+import '../scss/styles.scss';
 
-const messageInput = document.getElementById('messageInput');
+const html = document.documentElement;
 
-// messageInput.addEventListener('input', (e) => {
-//     e.target.style.height = e.target.scrollHeight + 'px';
-// })
+const storedThemeValue = localStorage.getItem('lightTheme');
+
+if (storedThemeValue === 'true') {
+    switchTheme(true);
+} else if (storedThemeValue === 'false') {
+    switchTheme(false);
+} else {
+    switchTheme(!osColorScheme());
+}
+
+function switchTheme(lightTheme) {
+    if (lightTheme) {
+        html.classList.remove("theme-light");
+        localStorage.setItem('lightTheme', lightTheme);
+    } else {
+        html.classList.add("theme-light");
+        localStorage.setItem('lightTheme', lightTheme);
+    }
+}
+
+function isLight() {
+    return html.classList.contains("theme-light");
+}
+
+function osColorScheme() {
+    return window.matchMedia("(prefers-color-scheme: light)").matches;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeSwitcher = document.getElementById("themeSwitcher");
+
+    themeSwitcher.addEventListener("click", () => {
+        switchTheme(isLight());
+    });
+
+    window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', e => {
+        switchTheme(!e.matches);
+    });
+});
