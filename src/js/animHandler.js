fa79a81ea68fa51d04b4b5e503180ff92cf612e9
liv3rise <liv3rise@gmail.com>
2023-02-23 15:41:39 +0200

    added animation when sending a message through the form

--- src/js/animHandler.js
+++ src/js/animHandler.js
@@ -1,18 +1,43 @@
 import { gsap } from "gsap";
 
-const glasses = document.getElementById('glasses');
-const lensHighlights = [document.getElementById('right-lens-h'), document.getElementById('left-lens-h')];
-
 export function moveGlasses(y, opacity, scale, duration) {
-    gsap.to(glasses, {
+    gsap.to('#glasses', {
         y,
         ease: 'bounce',
         duration,
     });
 
-    gsap.to(lensHighlights, {
+    gsap.to(['right-lens-h', 'left-lens-h'], {
         opacity,
         duration: duration - 0.2,
         scale
     });
-} 
+}
+
+export function openMailbox() {
+    gsap.set('#lid', {
+        transformOrigin: 'center'
+    });
+
+    gsap.set(['#flag'], {
+        transformOrigin: '25px 25px'
+    })
+
+    gsap.fromTo('#envelope', { x: 100 }, {
+        x: 0,
+        delay: 0.5,
+        duration: 1
+    })
+
+    gsap.to('#flag', {
+        rotate: -90,
+        duration: 1,
+        delay: 1.3
+    })
+
+    gsap.to('#lid', {
+        scale: 0.1,
+        opacity: 0,
+        duration: 0.3,
+    });
+}
---
+++ src/js/formHandler.js
@@ -0,0 +1,24 @@
+import { openMailbox } from "./animHandler";
+
+export default function handler() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => {
+                openMailbox();
+            })
+            .catch((error) => alert(error));
+    };
+
+    document
+        .querySelector("form")
+        .addEventListener("submit", handleSubmit);
+}
